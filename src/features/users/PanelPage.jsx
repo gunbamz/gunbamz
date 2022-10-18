048b7875a59e7ed4f180cb7457a4bd8b62e46384
gunbamz <muyiwaoluleye06@gmail.com>
2022-10-18 17:28:41 +0100

    portfolio project

--- src/features/users/PanelPage.jsx
+++ src/features/users/PanelPage.jsx
@@ -1,147 +1,65 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { NavLink, Outlet } from "react-router-dom";
 import { setMode } from "../../redux/modeRedux";
-import { BiSearch } from "react-icons/bi";
-import { FaClinicMedical } from "react-icons/fa";
-import { BiVial } from "react-icons/bi";
-import { BiBellPlus } from "react-icons/bi";
-import { BiBook } from "react-icons/bi";
-import { BiCreditCard } from "react-icons/bi";
-import { BiFirstAid } from "react-icons/bi";
-import { BiPieChartAlt2 } from "react-icons/bi";
-import { BiLogOutCircle } from "react-icons/bi";
-import { BiBandAid } from "react-icons/bi";
-import { BiPulse } from "react-icons/bi";
-import { BiUser } from "react-icons/bi";
-import { BiHotel } from "react-icons/bi";
-import { BiChevronRight } from "react-icons/bi";
+import profile from "../../assets/profile.jpg";
 import "./PanelPage.css";
 
 const PanelPage = () => {
-    const [isClose, setClose] = useState(false);
     const dispatch = useDispatch();
-    const { currentUser } = useSelector((state) => state.auth);
-    const handleChevronClick = () => {
-        setClose(!isClose);
-    }
-    const handleSearchClick = () => {
-        setClose(!isClose);
-    }
     const modeHandler = (e) => {
         let temp = e.target.checked;
         dispatch(setMode(temp));
     };
     return (
         <div className="panel-wrapper">
-            <nav className={isClose ? "sidebar close" : "sidebar"}>
+            <nav className="sidebar">
                 <header>
                     <div className="image-text">
                         <span className="image">
-                            <img src="logo.png" alt="" />
+                            <img src={profile} alt="" />
                         </span>
                         <div className="text logo-text">
-                            <span className="name">Welcome!</span>
-                            <span className="profession">Title Name</span>
-                            <span className="logo-text-clip">{currentUser.email}</span>
+                            <span className="profession">Muyiwa Ogunbameru</span>
+                            <span className="logo-text-clip">muyiwaoluleye06@gmail.com</span>
                         </div>
                     </div>
-                    <BiChevronRight onClick={handleChevronClick} className="toggle" />
                 </header>
                 <div className="menu-bar">
                     <div className="menu">
-                        <li className="search-box">
-                            <BiSearch onClick={handleSearchClick} className="icon" />
-                            <input type="text" placeholder="Search..." />
-                        </li>
                         <ul className="menu-links">
                             <li className="nav-link">
-                                 <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/records">
-                                    <BiBook className="icon" />
-                                    <span className="text nav-text">Records</span>
+                                 <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/about">
+                                    <span className="text nav-text">About</span>
                                  </NavLink>
                             </li>
                              <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/pharmacy">
-                                    <BiBandAid className="icon" />
-                                    <span className="text nav-text">Pharmacy</span>
+                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/qualifications">
+                                    <span className="text nav-text">Qualifications</span>
                                  </NavLink>
                             </li>
                             <li className="nav-link">
-                                 <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/appointment">
-                                    <BiBellPlus className="icon" />
-                                    <span className="text nav-text">Appointment</span>
+                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/skills">
+                                    <span className="text nav-text">Skill</span>
                                  </NavLink>
                             </li>
                             <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/clinics">
-                                    <BiPulse className="icon" />
-                                    <span className="text nav-text">Clinics</span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                 <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/wards">
-                                    <BiHotel className="icon" />
-                                    <span className="text nav-text">Wards</span>
+                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/portfolio">
+                                    <span className="text nav-text">Portfolio</span>
                                  </NavLink>
                             </li>
                             <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/theatre">
-                                    <FaClinicMedical className="icon" />
-                                    <span className="text nav-text">Theatre</span>
+                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/contact">
+                                    <span className="text nav-text">Contact</span>
                                  </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/laboratory">
-                                    <BiVial className="icon" />
-                                    <span className="text nav-text">Laboratory</span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/store">
-                                   <BiFirstAid className="icon" />
-                                   <span className="text nav-text">Store</span>
-                                 </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                 <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/accounting">
-                                    <BiCreditCard className="icon" />
-                                    <span className="text nav-text">Accounting</span>
-                                 </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/analytics">
-                                   <BiPieChartAlt2 className="icon" />
-                                   <span className="text nav-text">Analytics</span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/profiles">
-                                  <BiUser className="icon" />
-                                  <span className="text nav-text">Profiles</span>
-                                </NavLink>
                             </li>
                         </ul>
                     </div>
-                    <div className="bottom-content">
-                        <li className="nav-link">
-                            <NavLink className={(navData) => navData.isActive ? "active" : "" } to="/panel/logout">
-                                <BiLogOutCircle className="icon" />  
-                                <span className="text nav-text">Logout</span>
-                            </NavLink>
-                        </li>
-                        <li className="mode">
-                            <div className="sun-moon">
-                                <i className="bx bx-moon icon moon"></i>
-                                <i className="bx bx-sun icon sun"></i>
-                            </div>
-                            <span className="mode-text text">Dark mode</span>
-
-                            <label className="toggle-switch">
-                               <input type="checkbox" name="toggle" onClick={modeHandler} />
-                               <span className="switch"></span>
-                            </label>
-                        </li>
+                    <div className="lower-group">
+                        <span className="mode-text text">Mode</span>
+                        <label className="toggle-switch">
+                            <input type="checkbox" name="toggle" onClick={modeHandler} />
+                            <span className="switch"></span>
+                        </label>
                     </div>
                 </div>
             </nav>
@@ -152,4 +70,3 @@ const PanelPage = () => {
     )
 }
 export default PanelPage;
-// <div className="text">Dashboard Sidebar</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.jpg";
 import img4 from "../assets/img4.jpg";
-import "./Home.css"
+import "./Home.css";
 
 const Home = () => {
   return (
